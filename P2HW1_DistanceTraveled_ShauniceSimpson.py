5253ee46ac14f9eec44dfbc5c0bbde735ba692f1
simpsons8370 <78452580+simpsons8370@users.noreply.github.com>
2021-03-03 16:52:28 -0500

    Add files via upload
    Distance Traveled Submission

---
+++ P2HW1_DistanceTraveled_ShauniceSimpson.py
@@ -0,0 +1,20 @@
+#Program calculating distanced traveled by car
+#March 3, 2021
+#CTI-110 P2HW1 - Distance Traveled
+#Shaunice Simpson
+#
+
+car_speed = float(input('Enter car\'s speed: '))
+time_traveled = float(input('Enter time traveled: '))
+print()
+print('Speed entered: ', car_speed)
+print('Time entered: ', time_traveled)
+print()
+distance_traveled = car_speed * time_traveled
+print('Distance Traveled', distance_traveled, 'miles')
+
+#User inputs the car speed
+#User inputs the time traveled
+#Program displays the speed entered by user
+#Program displays the time traveled by user
+#Program multiples the speed and time and displays the product in miles

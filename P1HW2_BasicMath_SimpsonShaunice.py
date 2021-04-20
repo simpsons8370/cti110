34246936a30590f818d9d3e77497caabcea5142e
simpsons8370 <78452580+simpsons8370@users.noreply.github.com>
2021-04-20 16:00:34 -0400

    Add files via upload
    Basic Math submission

---
+++ P1HW2_BasicMath_SimpsonShaunice.py
@@ -0,0 +1,21 @@
+#Basic math program that adds and multiplies two numbers 
+#February 14, 2021
+#CTI-110 P1HW2 - Basic Math
+#Shaunice Simpson
+#
+
+print('Please Enter first number: ', end=' ')
+first_num = int(input())
+print('Please Enter second number: ', end=' ')
+second_num = int(input())
+print('First number entered: ', first_num)
+print('Second number entered: ', second_num)
+print()
+print('Sum of both numbers: ', first_num + second_num)
+print('Result of Multiplying the two numbers: ', first_num * second_num)
+
+#User inputs the first number
+#User inputs a second number
+#Program displays the first and second number the user input
+#Program adds the numbers the user input and displays the sum
+#Program multiplies the numbers the user input and displays the product
